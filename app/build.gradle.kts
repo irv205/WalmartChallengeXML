81f6ef8ae0f80a0fd633dac83452de8c60b9f239
Irving Gonzalez <gonzalez.irving.1994@outlook.com>
2024-10-10 21:15:52 -0500

    Removing unnecessary libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,9 +60,6 @@ dependencies {
         implementation("com.google.dagger:hilt-android:2.51.1")
         kapt("com.google.dagger:hilt-android-compiler:2.51.1")
 
-        // Glide
-        implementation("com.github.bumptech.glide:glide:4.15.0")
-
         // Network & GSON
         implementation("com.squareup.retrofit2:retrofit:2.9.0")
         implementation("com.squareup.okhttp3:okhttp:4.10.0")
